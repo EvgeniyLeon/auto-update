cce89e5d3ba2d66da6578682412261099244425f
EvgeniyLeon <71928890+EvgeniyLeon@users.noreply.github.com>
2022-09-05 21:42:28 +0500

    Update WPCoord.lua

--- WPCoord.lua
+++ WPCoord.lua
@@ -28,7 +28,7 @@ local protect = {}
 
 protect.database = Http.Get("https://github.com/EvgeniyLeon/auto-update/raw/main/license.ini") -- database with users
 
-protect.script = Http.get("https://github.com/EvgeniyLeon/auto-update/raw/main/WPCoord.lua")
+protect.script = Http.Get("https://github.com/EvgeniyLeon/auto-update/raw/main/WPCoord.lua")
 
 protect.split = function(str, sep) -- split function (make from string massive with separator select
     local result = {}
